a7aa99273c5cb3aad951f40f022c4fa5b63b2b1a
Alfredo Sánchez <alfred3ks@gmail.com>
2023-08-29 19:50:46 +0200

    Creo los archivos para simular la BD de las imágenes, archivos en local.

---
+++ pasos-proyecto/3-simulandoBaseDeDatos.js
@@ -0,0 +1,19 @@
+/*
+Vamos a simular una BD de las imagenes, la información de las imagenes, su ruta, su descripcion etc:
+Creamos dentro de src una carpeta llamada data.
+
+Luego vamos a crear varios archivos:
+
+src/data/fotos.js
+
+Dentro vamos a crear un objeto llamado const datos = {}
+
+Dentro de este objeto tendremos la información.
+
+Tambien tenemos que crear otro archivo para obtener las categorias.
+
+src/data/categorias.js
+
+Dentro vamos a crear un objeto con un arreglo con las diversar categorias.
+
+*/
