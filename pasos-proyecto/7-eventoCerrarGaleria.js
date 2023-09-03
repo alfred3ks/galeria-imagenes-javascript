ad8057686c0b72f2eada7f1d67d54a0bd67d63e1
Alfredo Sánchez <alfred3ks@gmail.com>
2023-09-03 12:53:24 +0200

    Agrego la información como cerrar la galeria, pasos del proyecto.

--- pasos-proyecto/7-eventoCerrarGaleria.js
+++ pasos-proyecto/7-eventoCerrarGaleria.js
@@ -18,4 +18,47 @@ src/galeria/eventoGaleria.js
 
 Tambien tenemos que ver que usamos el método closest(), para buscar elementos de abajo hacia arriba y encontrar el que le pasamos por parametro. Recordar que esto se hace porque estamos haciendolo con delegacion de evento, estamos colocando un evento a la galeria en ves de colocar un evento por elemento.
 
+Aqui vemos que para cerrar la galeria he creado una funcion externa para modularizar mejor las funciones a aplicar.
+
 */
+
+// 🎯 Cerrar la galeria: Funcion externa:
+// Accedemos a la galeria:
+const galeria = document.getElementById('galeria');
+
+// Creamos una funcion para cerrar la galeria:
+const cerrarGaleria = () => {
+  galeria.classList.remove('galeria--active');
+  // Devolvemos el scroll a la página:
+  document.body.style.overflow = '';
+};
+
+export default cerrarGaleria;
+
+// 🎯 Eventos a galeria:
+// const galeria = document.getElementById('galeria');
+import cerrarGaleria from './cerrarGaleria';
+
+// Creamos el evento:
+galeria.addEventListener('click', (e) => {
+  // Vemos toda la galeria:
+  // console.log(e.target);
+
+  // Vamos a detectar el boton: Cada boton tiene un atributo personalizado asi accederemos a el:
+  // Usamos el método closest()
+  // El método closest() busca de abajo hacia arriba hasta que encuentra lo que le pasamos por parametro.
+  // console.log(e.target.closest('button'));
+  const botonPulsado = e.target.closest('button');
+
+  // Con dataset podemos acceder a los atributos personalizados que comienzan con el nombre data-algo:
+  // Aqui vemos algo interesante, el ?, si ese elemento tiene la propiedad dataset, lo pongo porque da error si no lo lleva al pulsar sobre otro elemento que no tiene esa propiedad:
+  if (botonPulsado?.dataset?.accion === 'cerrar-galeria') {
+    console.log(botonPulsado.dataset.accion);
+
+    // Buscamos dentro de la galeria un boton que tenga esa clase:
+    // Aplicamos el estilos para cerrar la galeria: Dos formas aqui directamente o por medio de una función externa:
+    // galeria.classList.remove('galeria--active');
+    // document.body.style.overflow = ('');
+    cerrarGaleria();
+  }
+});
